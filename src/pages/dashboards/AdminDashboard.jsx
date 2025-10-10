7df5b27f68efda6a08d5350664761318b56e8a04
Sreenija <118006659+SreenijaPuvvula@users.noreply.github.com>
2025-10-10 23:54:58 +0530

    Update AdminDashboard.jsx

--- src/pages/dashboards/AdminDashboard.jsx
+++ src/pages/dashboards/AdminDashboard.jsx
@@ -399,10 +399,7 @@ export default function AdminDashboard() {
                 setPageUser(1);
               }}
             />
-          </div>
-
-          <div className="overflow-x-auto">
-            {usersPagination.isLoading ? (
+             {usersPagination.isLoading ? (
               <div className="flex justify-center items-center py-6 text-blue-600">
                 <Loader2 className="animate-spin w-5 h-5 mr-2" /> Loading
               </div>
