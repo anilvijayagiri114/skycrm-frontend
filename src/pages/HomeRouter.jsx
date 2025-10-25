1a7a2613eb6a62d23855124359ed83d5cb5b33f7
manu1242 <yallamanohar74@gmail.com>
2025-10-25 13:16:07 +0530

    Add React Lazy for Increase the Performance

--- src/pages/HomeRouter.jsx
+++ src/pages/HomeRouter.jsx
@@ -1,21 +1,24 @@
+import React, { useState, Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { getUserFromToken } from "../utils/auth";
-import { useState } from "react";
 import Header from "../components/Header";
-import AdminDashboard from "./dashboards/AdminDashboard.jsx";
-import ManagerDashboard from "./dashboards/ManagerDashboard.jsx";
-import TeamLeadDashboard from "./dashboards/TeamLeadDashboard.jsx";
-import SalesRepDashboard from "./dashboards/SalesRepDashboard.jsx";
-import LeadDetailPage from "./LeadDetailPage.jsx";
-import RegisterUser from "../components/RegisterUser.jsx";
-import ChangePassword from "./ChangePassword.jsx";
-import ForgotPassword from "./ForgotPassword.jsx";
-import ManageUsers from "../components/ManageUsers.jsx";
-import ActivityLogs from '../components/ActivityLogs.jsx'
+
+
+const AdminDashboard = lazy(() => import("./dashboards/AdminDashboard.jsx"));
+const ManagerDashboard = lazy(() => import("./dashboards/ManagerDashboard.jsx"));
+const TeamLeadDashboard = lazy(() => import("./dashboards/TeamLeadDashboard.jsx"));
+const SalesRepDashboard = lazy(() => import("./dashboards/SalesRepDashboard.jsx"));
+const LeadDetailPage = lazy(() => import("./LeadDetailPage.jsx"));
+const RegisterUser = lazy(() => import("../components/RegisterUser.jsx"));
+const ChangePassword = lazy(() => import("./ChangePassword.jsx"));
+const ForgotPassword = lazy(() => import("./ForgotPassword.jsx"));
+const ManageUsers = lazy(() => import("../components/ManageUsers.jsx"));
+const ActivityLogs = lazy(() => import("../components/ActivityLogs.jsx"));
 
 export default function HomeRouter() {
   const user = getUserFromToken();
   const role = user?.roleName;
+
   const base =
     role === "Admin"
       ? "/admin"
@@ -32,26 +35,35 @@ export default function HomeRouter() {
       {/* Sidebar */}
       <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      {/* Main content (shift right when sidebar is open) */}
+      {/* Main content */}
       <main
         className={`flex-1 transition-all duration-300 p-6 ${
           sidebarOpen ? "ml-60" : "ml-0 md:ml-20"
         }`}
       >
         <div className="max-w-10xl mx-auto text-gray-900 dark:text-gray-100">
-          <Routes>
-            <Route path="/" element={<Navigate to={base} replace />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/manager" element={<ManagerDashboard />} />
-            <Route path="/teamlead" element={<TeamLeadDashboard />} />
-            <Route path="/rep" element={<SalesRepDashboard />} />
-            <Route path="/leads/:id" element={<LeadDetailPage />} />
-            <Route path="/registerUser" element={<RegisterUser />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/manageUsers" element={<ManageUsers />} />
-            <Route path="/activityLogs" element={<ActivityLogs/>}/>
-          </Routes>
+         
+          <Suspense
+            fallback={
+              <div className="flex justify-center items-center h-[60vh] text-lg font-medium">
+                Loading...
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Navigate to={base} replace />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/manager" element={<ManagerDashboard />} />
+              <Route path="/teamlead" element={<TeamLeadDashboard />} />
+              <Route path="/rep" element={<SalesRepDashboard />} />
+              <Route path="/leads/:id" element={<LeadDetailPage />} />
+              <Route path="/registerUser" element={<RegisterUser />} />
+              <Route path="/change-password" element={<ChangePassword />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/manageUsers" element={<ManageUsers />} />
+              <Route path="/activityLogs" element={<ActivityLogs />} />
+            </Routes>
+          </Suspense>
         </div>
       </main>
     </div>
